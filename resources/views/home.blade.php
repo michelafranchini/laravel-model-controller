7f3bc5d1b9d0ea03e9b27d11fa3041899f953893
michelafranchini <michela.franchini.91@gmail.com>
2021-07-14 14:58:08 +0200

    Controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Model Controller</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+       
+    </head>
+    <body>
+        <h1>ciao!</h1>
+        {{-- @foreach ($movies as $movie)
+            <h2>{{$movie->title}}</h2>
+            <h3>{{$movie->original_title}}</h3>
+        @endforeach --}}
+    </body>
+</html>
